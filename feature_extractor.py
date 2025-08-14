5d6c55393f1fb85b0b4079c47772ba427f5dfc4d
kirilllzaitsev <kzaitsev88@gmail.com>
2025-08-14 23:46:39 +0200

    m:add HybridFeatureExtractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -38,6 +38,22 @@ class TransformerFeatureExtractor(nn.Module):
         return [f[:, 1:] for f in features.values()]
 
 
+class HybridFeatureExtractor(nn.Module):
+    def __init__(self, cnn_backbone=None, transformer_backbone=None):
+        super().__init__()
+
+        self.cnn_backbone = cnn_backbone
+        self.transformer_backbone = transformer_backbone
+
+        self.cnn_extractor = CNNFeatureExtractor(cnn_backbone)
+        self.transformer_extractor = TransformerFeatureExtractor(transformer_backbone)
+
+    def forward(self, x):
+        cnn_features = self.cnn_extractor(x)[:2]
+        transformer_features = self.transformer_extractor(x)[2:]
+        return cnn_features + transformer_features
+
+
 if __name__ == "__main__":
     extractor = CNNFeatureExtractor().cuda()
     inp = torch.randn(1, 3, 256, 256).cuda()
